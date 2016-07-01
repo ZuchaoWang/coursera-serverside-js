6c347a6a294640c408b191c48fd81654dd875188
zuchao.wang <zuchao.wang@gmail.com>
2016-07-01 23:52:24 +0800

    week4: population and https

--- week4/rest-server-passport/routes/leaderRouter.js
+++ week4/rest-server-passport/routes/leaderRouter.js
@@ -3,21 +3,22 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 var Leadership = require('../models/leadership');
-var Verify    = require('./verify');
+var Verify = require('./verify');
 
 var leaderRouter = express.Router();
 leaderRouter.use(bodyParser.json());
 
 leaderRouter.route('/')
-  .get(Verify.verifyOrdinaryUser, function (req, res, next) {
-    Leadership.find({}, function (err, leader) {
+  .all(Verify.verifyOrdinaryUser)
+  .get(function(req, res, next) {
+    Leadership.find({}, function(err, leader) {
       if (err) throw err;
       res.json(leader);
     });
   })
 
-.post(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
-  Leadership.create(req.body, function (err, leader) {
+.post(Verify.verifyAdmin, function(req, res, next) {
+  Leadership.create(req.body, function(err, leader) {
     if (err) throw err;
     console.log('Leader created!');
     var id = leader._id;
@@ -29,34 +30,35 @@ leaderRouter.route('/')
   });
 })
 
-.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
-  Leadership.remove({}, function (err, resp) {
+.delete(Verify.verifyAdmin, function(req, res, next) {
+  Leadership.remove({}, function(err, resp) {
     if (err) throw err;
     res.json(resp);
   });
 });
 
 leaderRouter.route('/:leaderId')
-  .get(Verify.verifyOrdinaryUser, function (req, res, next) {
-    Leadership.findById(req.params.leaderId, function (err, leader) {
+  .all(Verify.verifyOrdinaryUser)
+  .get(function(req, res, next) {
+    Leadership.findById(req.params.leaderId, function(err, leader) {
       if (err) throw err;
       res.json(leader);
     });
   })
 
-.put(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
+.put(Verify.verifyAdmin, function(req, res, next) {
   Leadership.findByIdAndUpdate(req.params.leaderId, {
     $set: req.body
   }, {
     new: true
-  }, function (err, leader) {
+  }, function(err, leader) {
     if (err) throw err;
     res.json(leader);
   });
 })
 
-.delete(Verify.verifyOrdinaryUser, Verify.verifyAdmin, function (req, res, next) {
-  Leadership.findByIdAndRemove(req.params.leaderId, function (err, resp) {
+.delete(Verify.verifyAdmin, function(req, res, next) {
+  Leadership.findByIdAndRemove(req.params.leaderId, function(err, resp) {
     if (err) throw err;
     res.json(resp);
   });
--- week4/rest-server-passport/routes/users.js
+++ week4/rest-server-passport/routes/users.js
@@ -2,25 +2,40 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var User = require('../models/user');
-var Verify    = require('./verify');
+var Verify = require('./verify');
 
 /* GET users listing. */
 router.get('/', Verify.verifyOrdinaryUser, Verify.verifyAdmin, function(req, res, next) {
-  User.find({}, function (err, users) {
-      if (err) throw err;
-      res.json(users);
+  User.find({}, function(err, users) {
+    if (err) throw err;
+    res.json(users);
   });
 });
 
 router.post('/register', function(req, res) {
-    User.register(new User({ username : req.body.username }),
-      req.body.password, function(err, user) {
-        if (err) {
-            return res.status(500).json({err: err});
-        }
-        passport.authenticate('local')(req, res, function () {
-            return res.status(200).json({status: 'Registration Successful!'});
+  User.register(new User({
+      username: req.body.username
+    }),
+    req.body.password,
+    function(err, user) {
+      if (err) {
+        return res.status(500).json({
+          err: err
         });
+      }
+      if (req.body.firstname) {
+        user.firstname = req.body.firstname;
+      }
+      if (req.body.lastname) {
+        user.lastname = req.body.lastname;
+      }
+      user.save(function(err, user) {
+        passport.authenticate('local')(req, res, function() {
+          return res.status(200).json({
+            status: 'Registration Successful!'
+          });
+        });
+      });
     });
 });
 
@@ -42,17 +57,17 @@ router.post('/login', function(req, res, next) {
       }
 
       var token = Verify.getToken(user);
-              res.status(200).json({
+      res.status(200).json({
         status: 'Login successful!',
         success: true,
         token: token
       });
     });
-  })(req,res,next);
+  })(req, res, next);
 });
 
 router.get('/logout', function(req, res) {
-    req.logout();
+  req.logout();
   res.status(200).json({
     status: 'Bye!'
   });
